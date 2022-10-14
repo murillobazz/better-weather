37053bb595daf43b4df851fadafe5cf40723a854
murillobazz <murillobazilio@gmail.com>
2022-10-14 20:35:23 -0300

    Adiciona componente Search

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'bcd176bd6bmsh8bc63ac48d6a840p10ef50jsneb9a1581c282',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
